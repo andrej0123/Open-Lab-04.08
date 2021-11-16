e016252d21eaad3ba86ed98dd56522ef5ace754a
Andrej Svrček <nepoviem251@gmail.com>
2021-11-16 17:34:57 +0100

    method that returns the strings from the previous array which are 4 letters long

--- Open-Lab-04.08/StringTools.cs
+++ Open-Lab-04.08/StringTools.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Open_Lab_04._08
 {
@@ -6,7 +8,18 @@ namespace Open_Lab_04._08
     {
         public string[] IsFourLetters(string[] strings)
         {
-            throw new NotImplementedException();
+            var vs = new List<string>();
+
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (strings[i].Length == 4)
+                {
+                    vs.Add(strings[i]);
+                }
+            }
+            string[] answer = vs.ToArray();
+            return answer;
         }
     }
 }
